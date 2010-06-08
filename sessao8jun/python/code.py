c56421cc983d1bc0164ffc08e137f248ceaf4c5a
Marcio Amado Duarte Santana <marcio.santana@sargento.glb.com>
2010-06-08 14:36:40 -0300

    Incluindo código python para Kata de OCR.

--- sessao8jun/python/code.py
+++ sessao8jun/python/code.py
@@ -2,3 +2,23 @@
 # -*- coding: utf-8 -*-
 
 '''Módulo de código da solução do kata do globodojo da sessão do dia 8 de junho de 2010.'''
+
+def monta_dado(entrada):
+    retorno = []
+    linha = ''
+    count = 0
+    for letra in entrada:
+        linha += letra
+        count += 1
+        if count == 27:
+            retorno.append(linha)
+            linha = ''
+            count = 0
+    
+    return retorno
+    
+def le_dados(dados):
+    
+    item = dados[0][:3] + dados[1][:3] + dados[2][:3] + dados[3][:3]
+    
+    return ""
--- sessao8jun/python/test.py
+++ sessao8jun/python/test.py
@@ -4,6 +4,34 @@
 '''Módulo de testes da solução do kata do globodojo da sessão do dia 8 de junho de 2010.'''
 
 import code
+from ocr_input import *
+from nose.tools import assert_equals
 
-def test_alguma_coisa():
-    assert True
+def test_montagem_dos_dados_de_input():
+    assert len(code.monta_dado(test_data_0)) == 4, '%s' % len(code.monta_dado(test_data_0))
+
+def test_obtem_zero():
+    dados = code.monta_dado(test_data_0)
+    resultado = code.le_dados(dados)
+    assert_equals(resultado, result_0)
+
+def test_le_dados_retorna_array_com_dados_nulos():
+    dados = [
+    ' _  _  _  _  _  _  _  _  _ ',
+    '| || || || || || || || || |',
+    '|_||_||_||_||_||_||_||_||_|',
+    '                           ']
+    resultado = code.le_dados(dados)
+    
+    assert_equals(resultado[0], '0')
+    
+def test_le_dados_retorna_array_com_dados_retorna_1():
+    dados = [
+    '                           ',
+    '  |  |  |  |  |  |  |  |  |',
+    '  |  |  |  |  |  |  |  |  |',
+    '                           ',]
+    
+    resultado = code.le_dados(dados)
+    assert_equals(resultado[0], '1')
+
